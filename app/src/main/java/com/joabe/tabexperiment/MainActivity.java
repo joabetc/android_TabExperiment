46aaaa5010e30889f65d7823afd6af3d6d0b5fb3
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-03 14:09:41 -0300

    Inflating the Toolbar and TabLayout

--- app/src/main/java/com/joabe/tabexperiment/MainActivity.java
+++ app/src/main/java/com/joabe/tabexperiment/MainActivity.java
@@ -1,8 +1,11 @@
 package com.joabe.tabexperiment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar =
+                findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
 }
